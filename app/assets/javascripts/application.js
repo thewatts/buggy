d7dc179d246d59c9e66cccd51d338987b43eddb3
Nathaniel Watts <reg@nathanielwatts.com>
2013-11-22 17:24:21 -0700

    Backbone application bootstrapped

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require ./lib/underscore
+//= require ./lib/backbone
+//= require ./app/buggy
